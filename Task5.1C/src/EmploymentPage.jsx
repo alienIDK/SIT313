13e5daa73f99ca3c29aeda9b8cd4e0b4404fb55c
alienIDK <140614916+alienIDK@users.noreply.github.com>
2023-08-18 20:49:46 +1000

    Update (props section) EmploymentPage.jsx

--- Task5.1C/src/EmploymentPage.jsx
+++ Task5.1C/src/EmploymentPage.jsx
@@ -4,11 +4,26 @@ import OpportunityInput from './OpportunityInput';
 import './OpportunityInput.css';
 
 function EmploymentPage() {
+
+    const experienceSection = (
+        <div className="form-group">
+            <h2>Experience: </h2>
+            <div className="form-group">
+                    <label for="experience" >Experience in</label>
+                    <input type="text" id="experience" name="experience" />
+            </div>  
+            <div className="form-group">
+                    <label for="duration" >For at least</label>
+                    <input type="text" id="duration" name="experdurationience" />
+            </div>  
+        </div>
+    );
+
     return (
         <div className="page-container">
             
             <h1>Employment Opportunities</h1>
-            <OpportunityInput/>
+            <OpportunityInput additionalSection={experienceSection} />
             <div className="back-button">
                 <Link to="/">Go Back to Main Page</Link>
             </div>
@@ -18,4 +33,3 @@ function EmploymentPage() {
 }
 
 export default EmploymentPage;
-
